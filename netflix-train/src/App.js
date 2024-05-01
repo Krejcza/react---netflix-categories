b65f28e20ab2fb57efba8412e426e1978b90304c
Krejcza <155578815+Krejcza@users.noreply.github.com>
2024-05-01 20:45:31 +0200

    funkcionalita malé appky zprovozněna - připraveno na stylování

--- netflix-train/src/App.js
+++ netflix-train/src/App.js
@@ -1,7 +1,40 @@
+import {useState} from 'react'
+import allMovies from './data'
+import categories from './Categories'
+
 const App = () =>{
-  return(
-    <h2>Ahoj</h2>
-  )
+
+  const [typeOfMovie, setTypeOfMovie] = useState('komedie')
+
+  const vysledneFilmy = allMovies.filter((oneMovie)=>{
+    return oneMovie['category'] === typeOfMovie
+  })
+  
+  return <div>
+    <div className='all-buttons'>
+      {
+        categories.map((oneCategory, index) =>{
+          return <button key={index} onClick={()=> setTypeOfMovie(oneCategory)}>{oneCategory}</button>
+        })
+      }
+    </div>
+
+    <div className='all-movies'>
+      {
+        vysledneFilmy.map((mujFilm)=>{
+          const {id, image, title, age, tags, description} = mujFilm
+
+          return <div key={id} className='one-movie'>
+            <img src={image} alt="" />
+            <h2>{title}</h2>
+            <p>{age}</p>
+            <p>{tags}</p>
+            <p>{description}</p>
+          </div>
+        })
+      }
+      </div>
+  </div>
 }
 
 export default App
